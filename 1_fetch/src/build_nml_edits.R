90bbeb9fe7d01adaaf11f6a930645884ba130d8c
Alison Appling <aappling@usgs.gov>
2021-03-16 16:57:17 -0400

    remove comments, rename inout direction to location

--- 1_fetch/src/build_nml_edits.R
+++ 1_fetch/src/build_nml_edits.R
@@ -8,54 +8,19 @@ build_nml_edits <- function(inout_feather, releases_csv) {
   # a unique strmbd_slope for each inflow)
   inout_info <- inout_feather %>%
     arrow::read_feather() %>%
-    group_by(res_id, direction, seg_id_nat) %>%
+    rename(location = direction) %>%
+    group_by(res_id, location, seg_id_nat) %>%
     summarize(n_dates=n(), min_date=min(date), max_date=max(date), .groups='drop')
-  pep_inflows <- inout_info %>% filter(res_id == 'nhdhr_151957878', direction == 'inflow')
-  can_inflows <- inout_info %>% filter(res_id == 'nhdhr_120022743', direction == 'inflow')
-  # pep_outflows <- inout_info %>% filter(res_id == 'nhdhr_151957878', direction == 'outflow') # we'll use release data instead of PRMS predictions for outflows
-  # can_outflows <- inout_info %>% filter(res_id == 'nhdhr_120022743', direction == 'outflow') # we'll use release data instead of PRMS predictions for outflows
+  pep_inflows <- inout_info %>% filter(res_id == 'nhdhr_151957878', location == 'inflow')
+  can_inflows <- inout_info %>% filter(res_id == 'nhdhr_120022743', location == 'inflow')
+  # pep_outflows <- inout_info %>% filter(res_id == 'nhdhr_151957878', location == 'outflow') # we'll use release data instead of PRMS predictions for outflows
+  # can_outflows <- inout_info %>% filter(res_id == 'nhdhr_120022743', location == 'outflow') # we'll use release data instead of PRMS predictions for outflows
   release_info <- releases_csv %>%
     readr::read_csv(col_types=cols()) %>%
     group_by(reservoir, release_type) %>%
     summarize(n_dates=n(), min_date=min(date), max_date=max(date), .groups='drop')
   pep_releases <- release_info %>% filter(reservoir == 'Pepacton')
   can_releases <- release_info %>% filter(reservoir == 'Cannonsville')
-
-  # Data exploration
-  # pep_out <- inout_feather %>% arrow::read_feather() %>% filter(res_id == 'nhdhr_151957878', direction == 'outflow')
-  # can_out <- inout_feather %>% arrow::read_feather() %>% filter(res_id == 'nhdhr_120022743', direction == 'outflow')
-  # pep_rel <- releases_csv %>% readr::read_csv(col_types=cols()) %>% filter(reservoir == 'Pepacton') %>% mutate(flow=release_volume_cfs*0.028316847)
-  # can_rel <- releases_csv %>% readr::read_csv(col_types=cols()) %>% filter(reservoir == 'Cannonsville') %>% mutate(flow=release_volume_cfs*0.028316847)
-  # pep_rel_tot <- pep_rel %>%
-  #   group_by(date) %>%
-  #   summarize(flow = sum(flow), .groups='drop')
-  # can_rel_tot <- can_rel %>%
-  #   group_by(date) %>%
-  #   summarize(flow = sum(flow), .groups='drop')
-  # # Pepacton
-  # pep_rel %>% group_by(release_type) %>% summarize(sum(flow))
-  # # release_type `sum(flow)`
-  # # Conser            44956.
-  # # Direct            12480.
-  # # Spill             48538.
-  # ggplot(pep_rel, aes(x=date, y=flow, color=release_type)) + geom_line()
-  # pep_matchups <- select(pep_out, date, outflow_prms=seg_outflow) %>% inner_join(select(pep_rel_tot, date, outflow_releases=flow), by='date')
-  # pep_matchups %>% summarize(sum(outflow_prms), sum(outflow_releases))
-  # # `sum(outflow_prms)` `sum(outflow_releases)`
-  # #             222619.                 105974.    # big mismatch
-  # pep_matchups %>% ggplot(aes(x=outflow_prms, y=outflow_releases)) + geom_abline() + geom_point(alpha=0.5) + theme_bw() # plenty of daily mismatches
-  # # Cannonsville
-  # can_rel %>% group_by(release_type) %>% summarize(sum(flow))
-  # # release_type `sum(flow)`
-  # # Conser            88273.
-  # # Direct            58281.
-  # # Spill            101432.
-  # ggplot(can_rel, aes(x=date, y=flow, color=release_type)) + geom_line()
-  # can_matchups <- select(can_out, date, outflow_prms=seg_outflow) %>% inner_join(select(can_rel_tot, date, outflow_releases=flow), by='date')
-  # can_matchups %>% summarize(sum(outflow_prms), sum(outflow_releases))
-  # # `sum(outflow_prms)` `sum(outflow_releases)`
-  # #             254779.                 247986.    # much closer than pepacton
-  # can_matchups %>% ggplot(aes(x=outflow_prms, y=outflow_releases)) + geom_abline() + geom_point(alpha=0.5) + theme_bw() # still plenty of daily mismatches
 
   list(
     nhdhr_151957878 = list(
